e8b4d7d979a24f5cb1333fde8b0db22e998cffec
RuanMenezesLima123 <ruan.lima07@aluno.ifce.edu.br>
2025-01-29 17:06:53 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,38 +4,48 @@ var cont;
 function gerarQuadrados() {
 
     var input = parseInt(document.getElementById("inputEscolha").value);
+    // puxa o valor da quantidade de coluna
     var container = document.getElementById("container-main");
+    var spanContaErros = document.getElementById("contaErros");
 
     cont = 0;
     
-    container.innerHTML = "";
+    container.innerHTML = ""; 
+    // o que essa linha tá fazendo???
 
     var idElement = 1;
 
     for (let i = 1; i <= input; i++) {
-        for (let k = 1; k <= 3; k++) {
+        for(let k = 1; k <= 3; k++){
+            
             var box = document.createElement("div");
+            // aqui eu crio uma unidade de quadrado (div)
 
             box.setAttribute("class", "box");
             box.setAttribute("id", idElement);
 
-            box.addEventListener("click", AlterarBox);
+            box.addEventListener("click", alterarBox);
             //opção de tornar função anônima
 
             box.innerHTML = idElement;
-            // essa atribuição tá quebrando o id de cada box
 
             container.appendChild(box);
             idElement++;
         }
-
     }
 
     bxS = Math.floor(Math.random() * input) + 1;
 
+    console.log(bxS);
+    setInterval(function() {
+        spanContaErros.innerHTML = bxS;
+        bxS = Math.floor(Math.random() * input) + 1;
+        console.log(bxS);
+    }, 5000);
+
 }
 
-function AlterarBox() {
+function alterarBox() {
 
     var span = document.getElementById("spanErro");
 
